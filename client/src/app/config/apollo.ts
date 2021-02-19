188bd2c531758d8d6a59c172611bb993d9dd1636
Jaroslaw <yaroslav@xuver-services.com>
2021-02-19 08:10:27 +0200

    Add apollo-link-retry

--- client/src/app/config/apollo.ts
+++ client/src/app/config/apollo.ts
@@ -1,23 +1,41 @@
-import { ApolloClient,   NormalizedCacheObject,
-  createHttpLink, InMemoryCache } from '@apollo/client';
-import { onError } from "apollo-link-error";
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+import { onError } from 'apollo-link-error'
+import { RetryLink } from 'apollo-link-retry'
 
-
-const httpLink = createHttpLink({ uri: `${process.env.SERVER_API_URL}`, credentials: 'include', });
+const httpLink = createHttpLink({
+  uri: `${process.env.SERVER_API_URL}`,
+  credentials: 'include',
+})
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.forEach(({ message, locations, path }) =>
       console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
-    );
-  if (networkError) console.log(`[Network error]: ${networkError}`);
-});
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    )
+  if (networkError) console.log(`[Network error]: ${networkError}`)
+})
 
- 
+const retryLink = new RetryLink({
+  delay: {
+    initial: 300,
+    max: Infinity,
+    jitter: true,
+  },
+  attempts: {
+    max: 3,
+    retryIf: (error, _operation) => !!error,
+  },
+})
+
 export const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  credentials: "include",
-  link: errorLink.concat(httpLink),
-  cache: new InMemoryCache()
-});
+  credentials: 'include',
+  link: errorLink.concat(retryLink.concat(httpLink)),
+  cache: new InMemoryCache(),
+})
